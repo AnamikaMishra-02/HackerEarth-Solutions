7ac99f237a14c1d90a6e61ee2ce0e0e79c218724
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-08-30 12:51:42 +0530

    Python Program

---
+++ Jadoo-and-DNA-Transcription.py
@@ -0,0 +1,18 @@
+
+
+dna=input()
+new = ""
+for i in dna:
+    if i not in 'ATGC':
+      new="Invalid Input"
+      break
+    else:
+        if i=='A':
+            new += 'U'
+        elif i=='T':
+            new += 'A'
+        elif i=='C':
+            new += 'G'
+        elif i=='G':
+            new += 'C'
+print (new)
